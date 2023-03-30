8dc0031e53b24923fecfdb48f7544049339f9af2
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-03-30 14:08:32 +0530

    Update Countword.js

--- Countword.js
+++ Countword.js
@@ -1,3 +1,11 @@
+// 1. Count the occurrences.
+// You are building a word count generator that will take a large string of text as input and output the words and
+// the number of times they are present in the string. Your task is to write a function that can count the
+// occurrences of each word in the string and return a Map containing each word's count. For simplicity, you can
+// assume that the string only contains alphabetic characters and spaces.
+
+
+
 const text = "html css javascript javascript html css css";
 const wordCounts = countWords(text);
 console.log(wordCounts);
